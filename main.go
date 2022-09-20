91df67545caba17e73e83dfb307b358962f7fc22
Carl Hossner <carl.hossner@nord-ic.com>
2022-09-20 15:29:12 +0200

    Bumped to v2

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"net/http"
 )
 
-const demoVer = "1.1.19"
+const useTLS = false
+
+const demoVer = "2.0.0"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf("<h2>Ver: %s</h2>", demoVer)))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world, this is a demo, version %s\n", demoVer)
-	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	if useTLS {
+		http.ListenAndServeTLS(":8080", "/certs/demo_srvr_cert.pem", "/certs/demo_server_key.pem", nil)
+	} else {
+		http.ListenAndServe(":8080", nil)
+	}
 }
